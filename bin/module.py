e1ba9a6068c45c139a2e4eda90b104ed914e8c8e
verbalhanglider <tdanstrom@uchicago.edu>
2016-01-21 14:48:34 -0600

    added call for output in output instantiation; modified str(final_output.errors rendering out

--- bin/module.py
+++ bin/module.py
@@ -3,7 +3,7 @@ from uchicagoldr.output import Output
 from uchicagoldr.batch import StagingDirectory
 
 if __name__ == "__main__":
-    final_output = Output('a_type')
+    final_output = Output('a_type','a_preferred_output_format')
     directory_to_stage = StagingDirectory("/a/full/path/to/files/that/need/to/be/staged",
                                           "/a/full/path/to")
     final_output = directory_to_stage.validate(final_output)
@@ -13,5 +13,5 @@ if __name__ == "__main__":
         final_output.display()
     else:
         stderr.write("The following errors occured")
-        stderr.write(str(final_output.error_handler))
+        stderr.write(str(final_output.get_errors()))
     
