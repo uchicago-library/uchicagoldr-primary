91376246c1ea11422293d5ed8c32466c4c0352fd
Brian Balsamo <balsamo@uchicago.edu>
2016-01-21 12:15:00 -0600

    First go-over of the request class in detail.

--- uchicagoldr/request.py
+++ uchicagoldr/request.py
@@ -1,56 +1,85 @@
 class Request(object):
-    def __init__(self):
-        pass
+    def __init__(self, validator=None, prompt=None):
+        if validator is not None:
+            self.validator = validator
+        else:
+            self.validator = self._dummy_validator
 
-class InputRequestType(Request):
-    type = None
-    validate_function = None
+        self.prompt = prompt
 
-    def __init__(self, vtype):
-        self.type = vtype
-
-    def validate_function(self):
+    def _dummy_validator(self, response):
         return True
 
-    def validate(self):
-        return isinstance(self.value, self.type) and self.validate_function()
+    def validate(self, response):
+        return self.validator(response)
+
+
+class InputType(Request):
+    def __init__(self, vtype, validator=None, prompt=None):
+        if validator is None:
+            validator = self._validator
+        if prompt is None:
+            prompt = "Please supply a value of type: {}".format(str(vtype))
+        Request.__init__(self, validator, prompt=prompt)
+        self.expected_type = vtype
+
+    def _validator(self, response):
+        return isinstance(response, self.expected_type)
+
 
 class ChooseBetween(Request):
-    choices = []
-    valid = None
-
-    def __init__(self, options):
+    def __init__(self, options, validator=None, prompt=None):
+        if validator is None:
+            validator = self._validator
+        if prompt is None:
+            prompt = "Please make a single selection from the choices."
+        Request.__init__(self, validator, prompt=prompt)
         self.choices = options
 
-    def validate(value):
-        if value in choices:
+    def _validator(self, response):
+        if response in self.choices:
             return True
         else:
             return False
+
+
+class ChooseMultiple(Request):
+    def __init__(self, options, validator=None, prompt=None):
+        if validator is None:
+            validator = self._validator
+        if prompt is None:
+            prompt = "Please indicate a selection from the choices."
+        Request.__init__(self, validator, prompt=prompt)
+        self.choices = options
+
+    def _validator(self, response):
+        for entry in response:
+            if entry not in self.choices:
+                return False
+        return True
+
 
 class TrueOrFalse(Request):
-    valid = None
-    condition = False
+    def __init__(self, statement, validator=None, prompt=None):
+        if validator is None:
+            validator = self._validator
+        if prompt is None:
+            prompt = "Please indicate whether the statement is true or false."
+        Request.__init__(self, validator, prompt=prompt)
+        self.statement = statement
 
-    def __init__(self, condition):
-        self.condition = condition
+    def _validator(self, response):
+        return isinstance(bool, response)
 
-    def validate(value):
-        if bool(value) == condition:
-            return True
-        else:
-            return False
 
 class ConfirmRequest(Request):
-    valid = None
-    value = None
-    required = 'Y'
-
-    def __init__(self, value):
+    def __init__(self, value, validator=None, prompt=None):
+        if validator is None:
+            validator = self._validator
+        if prompt is None:
+            prompt = "Are you sure?"
+        Request.__init__(self, validator, prompt=prompt)
         self.value = value
 
-    def validate(self):
-        if self.value == required:
-            return True
-        else:
-            return False
+    def _validator(self, response):
+        return isinstance(bool, response)
