d1a8b0ceb868e1355e7a349ddf9d7bbb20aa4bd1
Brian Balsamo <balsamo@uchicago.edu>
2016-01-21 10:54:58 -0600

    Adding skeleton for interface agnostic classes.

--- uchicagoldr/outputhandler.py
+++ uchicagoldr/outputhandler.py
@@ -1,60 +1,60 @@
-from uchicagoldr.output import Outport
+from uchicagoldr.output import Output
 
-class Displayer(object):
-    def __init__(self, kind):
-	self.class_being_displayed = kind
 
-    def display():
-	if self.class_being_displayed == 'family':
-		return NotImplemented
-	if self.class_being_displayed == 'directory':
-		return NotImplemented
-	if self.class_being_displayed == 'string':
-		return NotImplemented
-	if self.class_being_displayed == 'item':
-		return NotImplemented
-	if self.class_being_displayed == 'accessionitem':
-		return NotImplemented
-    
-
-class OutputHandler(object)
-
-    kind = 'Abstract base class'
-
+class OutputHandler(object):
     def __init__(self, output_instance):
         assert(isinstance(output_instance, Output))
         self.output_instance = output_instance
-	displayer = Display(output_instance.type)
 
     def display_data(self):
+        displayer = Displayer(self.output_instance.type_str,
+                              self.output_instance.data)
         return displayer.display()
 
     def handle_requests(self):
-        raise NotImplemented
+        for request in self.requests:
+            handler = RequestHandler(request)
+            yield handler.handle_request()
 
     def handle_errors(self):
         raise NotImplemented
 
-    def _display_data_str(self):
-        raise NotImplemented
 
-    def _display_data_item(self):
-        raise NotImplemented
+class Displayer(object):
+    def __init__(self, kind, data):
+        self.class_being_displayed = kind
+        self.data = data
 
-    def _display_data_batch(self):
-        raise NotImplemented
+    def display(self):
+        if self.class_being_displayed == 'family':
+            return NotImplemented
+        if self.class_being_displayed == 'directory':
+            return NotImplemented
+        if self.class_being_displayed == 'string':
+            return NotImplemented
+        if self.class_being_displayed == 'item':
+            return NotImplemented
+        if self.class_being_displayed == 'accessionitem':
+            return NotImplemented
 
-    def _display_data_family(self):
-        raise NotImplemented
 
-    def _handle_request_true_or_false(self):
-        raise NotImplemented
+class RequestHandler(object):
+    def __init__(self, request):
+        self.request = request
 
-    def _handle_request_input(self):
-        raise NotImplemented
+    def handle_request(self):
+        return NotImplemented
 
-    def _handle_request_choose_between(self):
-        raise NotImplemented
+    def _handle_true_or_false(self):
+        return NotImplemented
 
-    def _handle_request_confirm(self):
-        raise NotImplemented
+
+class ErrorHandler(object):
+    def __init__(self, error):
+        self.error = error
+
+    def handle_error(self):
+        return NotImplemented
+
+    def _handle_value_error(self):
+        return NotImplemented
