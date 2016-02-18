c87dca0176940fafe33acb0b73dd52611fc1ef4c
verbalhanglider <tdanstrom@uchicago.edu>
2016-02-18 13:03:39 -0600

    starting staging processors code

--- uchicagoldr/stagingprocessors.py
+++ uchicagoldr/stagingprocessors.py
@@ -1,32 +1,35 @@
-
-class StagingProcessor(object):
-    def __init__(self, directory):
-        self.staging_directory = directory
-
-
-class PremisBuilder(StagingProcessor):
-
+from os import remove
+from uchicagoldr.filewalker import FileWalker
+from uchicagoldr.tree import FileProcessor
+        
+class PremisBuilder(FileProcessor):
     def __init__(self, directory):
         StagingProcessor.__init__(self, directory)
 
-class Pruner(StagingProcessor):
-
-    def __init__(self, directory, bad_files = []):
+class Pruner(FileProcessor):
+    def prune():
+        for a_file in self.tree.find_all_files():
+            for a_pattern in self.superfluous_file_patterns:
+                if re_compile(a_pattern).search(a_pattern):
+                    remove(a_file)
+        return 0
+    
+    def __init__(self, directory, bad_file_patterns):
         StagingProcessor.__init__(self, directory)
-        self.files_to_remove = bad_files
+        self.superfluous_file_patterns = bad_file_patterns
 
-class FileConverter(StagingProcessor):
+class FileConverter(FileProcessor):
 
     def __init__(self, directory):
         StagingProcessor(self, directory)
         self.conversion_lookup = None
 
-class TechnicalMetadataCreator(StagingProcessor):
+class TechnicalMetadataCreator(FileProcessor):
 
     def __init__(self, directory):
         StagingProcessor(self, directory)
 
-class Accessioner(StagingProcessor):
+class Accessioner(FileProcessor):
 
     def __init__(self, directory):
         StagingProcessor(self, directory)
--- uchicagoldr/tree.py
+++ uchicagoldr/tree.py
@@ -10,164 +10,9 @@ from sys import stdout, stderr
 from treelib import Tree, Node
 from uchicagoldr.filewalker import FileWalker
 from uchicagoldr.moveableitem import MoveableItem
+from uchicagoldr.walktree import FileWalkTree
 from datetime import datetime
 
-class LeafData(object):
-    """
-    attributes: filepath, filesize, filemimetype, filechecksum
-
-    methods: get_filepath, get_filesize, get_mimetype, get_checksum_options,
-    get_checksum
-    """
-
-    def __init__(self, filepath):
-        if not exists(filepath):
-            raise IOError("{} directory must exist on disk.".format(filepath))
-        if filepath[0] != '/':
-            self.filepath = abspath(filepath)
-        else:
-            self.filepath = filepath
-        self._derive_filesize()
-
-    def get_filepath(self):
-        return self.filepath
-
-    def get_filesize(self):
-        return self.filesize
-    
-    def _derive_filesize(self):
-        from os import stat
-        self.filesize = stat(self.filepath).st_size
-
-    def _derive_filemimetype(self):
-        try:
-            mimetype = guess_type(self.filepath)
-        except Exception as e:
-            mimetype = None
-        try:
-            mimetype = from_file(self.filepath, mime=True).decode('utf-8')
-        except Exception as e:
-            mimetype = None
-        self.filemimetype = mimetype
-
-    def __repr__(self):
-        return self.filepath 
-
-class WalkTree(object):
-    def __init__(self):
-        self.tree_root = None
-        self.expanded_node_list = None
-
-    def add_node(self, value, sep="/"):
-        value_parts = value.split(sep)[1:]
-        if not self.tree_root:
-            self.tree_root = Tree()
-            self.tree_root.create_node(value_parts[0],join('/',value_parts[0])) 
-        parent = self.tree_root.root
-        for position, value_part in enumerate(value_parts)[1:]:
-            self.tree_root.create_node(value_part, join(parent, value_part), parent=parent)
-
-    def remove_node(self, value):
-        self.tree_root.remove_node(value)
-
-    def get_all_nodes(self):
-        self._expand_node_list()
-        return self.expanded_node_list
-
-    def _expand_node_list(self):
-        if not self.expanded_node_list:
-            self.expanded_node_list = self.tree_root.all_nodes()
-        else:
-            return False
-        return True
-
-            
-class FileWalkTree(WalkTree):
-    """
-    attributes: tree_root, expanded_node_list
-
-    methods: add_node, remove_node, get_all_nodes, get_files, find_string_in_a_node_tag
-    """
-    tree_root = None
-    expanded_node_list = None
-    def __init_(self):
-        self.tree_root = None
-        self.expanded_node_list = None
-        
-    def add_node(self, value, irrelevant_parts = None):
-        if not irrelevant_parts:
-            value_parts = value.split('/')[1:]
-        else:
-            value_parts = value.split(irrelevant_parts)[1].split('/')
-        if not self.tree_root:
-            self.tree_root = Tree()
-            self.tree_root.create_node(value_parts[0],join('/',value_parts[0])) 
-        parent = self.tree_root.root
-        for position,value_part in enumerate(value_parts[1:]):
-            if position + 1 == len(value_parts[1:]):
-                if irrelevant_parts:
-                    data =  LeafData(irrelevant_parts+join(parent, value_part))
-                else:
-                    data = LeafData(join(parent, value_part))
-                self.tree_root.create_node(value_part, join(parent,value_part), parent=parent,data=data)
-                break
-            
-            elif self.tree_root.get_node(join(parent,value_part)):
-                pass
-
-            else:
-                self.tree_root.create_node(value_part, join(parent,value_part), parent=parent)             
-            parent = join(parent, value_part)
-            
-    def get_files(self):
-        return self.tree_root.leaves()
-
-    def is_it_a_subdirectory(self, n):
-        return not n.is_leaf()
-
-    def is_file_in_subdirectory(self, n, file_string):
-        print(n)
-        matches = [x for x in self.tree_root.get_node(n).fpointer \
-                   if file_string in n.identifier]
-        if matches:
-            return True
-        return False
-
-    def find_file_contents_of_a_subdirectory(self, n, all_files=[]):
-        matches = [x for x in self.tree.get_node(n).fpointer \
-                   if self.tree.get_node(x).is_leaf()]
-        
-        for x in self.tree.get_node(n).fpointer:
-            current = self.tree.get_node(x)
-            if current.is_leaf():
-                all_files.append(current)
-            if not current.is_leaf():
-                self.find_file_contents_of_a_subdirectory(x, all_files=all_files)
-        return all_matches
-    
-    def does_node_match_string(iself, n, id_string):
-        return n.identifier == id_string
-    
-    def find_string_in_a_node_tag(self, n, a_string):
-        if isinstance(n, Node):
-            return a_string in n.tag
-        else:
-            raise TypeError("must pass an object of type treelib.Node to the first parameter")
-
-    def trace_ancestry_of_a_node(self, a_node):
-        ancestry = [a_node]
-        while self.tree_root.parent(a_node.identifier):
-            ancestry.append(self.tree_root.parent(a_node.identifier))
-            a_node = self.tree_root.parent(a_node.identifier)
-        output = [n for n in reversed(ancestry)]
-        return output
-
-    def find_depth_of_a_node(self, a_node):
-        return self.tree_root.depth(a_node)
-    
-    def __repr__(self):
-        self.tree_root.show()
-        return ""
 
 class FileProcessor(object):
     """
@@ -197,7 +42,8 @@ class FileProcessor(object):
     def pattern_matching_files_regex(self, regex):
         matches = [x for x in self.get_tree().get_all_nodes() if \
                    re_compile(regex).search(x.tag) and x.is_leaf()]
-            
+	return matches    
+        
     def string_searching_files(self, val_string):
         matches =  [x for x in self.get_tree().get_files() if
                     self.get_tree().find_string_in_a_node_tag(x, val_string)]
@@ -451,8 +297,7 @@ class Stager(FileProcessor):
             except:
                 data_object = DataTransferObject(manifest_filepath,"null","null","null","Y")
             data_object.write_to_manifest(manifestwriter)
-        
-        
+                
 class Archiver(FileProcessor):
 
     """
@@ -599,7 +444,7 @@ class NewArchiver(FileProcessor):
     def validate(self):
         admin_node = self.find_subdirectory_at_particular_level_down('admin',3)
         data_node = self.find_subdirectory_at_particular_level_down('data', 3)
-
+        
         valid_admin = False
         for n in admin_node:
             current= admin_node.pop()
