4c202613805c55ea0f19730d0d6ea71e8d904bc1
verbalhanglider <tdanstrom@uchicago.edu>
2016-03-02 10:32:21 -0600

    starting to write out ingest() functionality. need to define ingest_premis_file and ingest_data_file

--- uchicagoldr/tree.py
+++ uchicagoldr/tree.py
@@ -183,7 +183,10 @@ class FileProcessor(object):
         if len([x for x in node.fpointer if file_name_string in x]) == 1:
             return True
         return False
-        
+
+    def find_files_in_a_subdirectory(a_node_name):
+        return [x for x in self.find_matching_node(a_node_name).fpointer if x.is_leaf()]
+    
     def get_tree(self):
         """
         This function returns the value of the tree attribute.
@@ -686,50 +689,114 @@ class NewArchiver(FileProcessor):
         self.destination_owner = user_id
 
     def validate(self):
-        admin_node = self.find_subdirectory_at_particular_level_down('admin',3)
-        data_node = self.find_subdirectory_at_particular_level_down('data', 3)
-        
+        admin_node = self.find_subdirectory_at_particular_level_down('admin',1)
+        data_node = self.find_subdirectory_at_particular_level_down('data', 1)
         valid_admin = False
         for n in admin_node:
+            n_premis_present = False
+            n_premis_match_with_data_files = False
             current= admin_node.pop()
             subdirs_in_current = self.find_directories_in_a_directory(current)
             if len(subdirs_in_current) == self.numfolders:
                 valid_admin = True
-                
-                premis_records = [x for x in subdirs_in_current if self.find_files_in_a_subdirectory(x, '*.premis.xml')]
-                for premis_record in premis_records:
-                    print(premis_record)
-                    
+            else:
+                valid_admin = False
+            n_premis_subdir_name = join(n.identifier,'premis')
+            if self.find_subdirectory_at_a_particular_level_down(n_premis_subdir_name, 3):
+                n_premis = True
+            else:
+                n_premis = False
+            if n_premis:
+                n_premis_file_count = len(self.find_files_in_a_subdirectory(n_premis_subdir_name))
+                n_data_file_count = len(self.find_files_in_a_subdirectory(join('data',join(n.identifier.split('/')[-1]))))
+                if n_premis_file_count == n_data_file_count:
+                    n_premis_match_with_data_files = True
+                else:
+                    n_premis_match_with_data_files = False
         valid_data = False
         for n in data_node:
             current = data_node.pop()
             subdirs_in_current = self.find_directories_in_a_directory(current)
             if len(subdirs_in_current) == self.numfolders:
                 valid_data = True
-    
-        return NotImplemented
+            else:
+                valid_data = False
+            if len(self.find_all_files()) == numfiles:
+                numfiles_equals_files_supposed_to_be = True
+            else:
+                numfiles_equals_files_supposed_to_be = False
+        return valid_admin & n_premis  & n_premis_match_with_data_files & valid_data & numfiles_equals_files_supposed_to_be
 
     def explain_validation_results(self):
-        admin_node = self.find_subdirectory_at_particular_level_down('admin', 3)
-        data_node = self.find_subdirectory_at_particular_level_down('data', 3)
-        if not admin_node:
-            return namedtuple("ldrerror","category message")("fatal","missing \"admin\" folder in correct position in this directory")            
-        elif not data_node:
-            return namedtuple("ldrerror","category message")("fatal","missing \"data\" folder in correct position in this directory")
-        if admin_node and data_node:
-            subdirs_in_admin = self.find_directories_in_a_directory(admin_node.pop())
-            subdirs_in_data = self.find_directories_in_a_directory(data_node.pop())
-            if len(subdirs_in_data) != len(subdirs_in_admin):
-                return namedtuple("lderrror","category message")("fatal","subdirectories of data and admin are not equal in number")
+        admin_node = self.find_subdirectory_at_particular_level_down('admin',1)
+        data_node = self.find_subdirectory_at_particular_level_down('data', 1)
+        valid_admin = False
+        for n in admin_node:
+            n_premis_present = False
+            n_premis_match_with_data_files = False
+            current= admin_node.pop()
+            subdirs_in_current = self.find_directories_in_a_directory(current)
+            if len(subdirs_in_current) != self.numfolders:
+                return namedtuple("ldererror","category message") \
+                    ("fatal","There are {} subdirectories in admin but there should be {}". \
+                     format(str(len(subdirs_in_current)),str(args.numfolders)))
 
-        return NotImplemented
+            n_premis_subdir_name = join(n.identifier,'premis')
+            if not self.find_subdirectory_at_a_particular_level_down(n_premis_subdir_name, 3):
+                return namedtuple("ldrerror", "category message") \
+                    ("fatal","There is no premis directory in {}".format(n_premis_subdir_name))
+            else:
     
+                n_premis_file_count = len(self.find_files_in_a_subdirectory(n_premis_subdir_name))
+                
+                n_data_file_count = len(self.find_files_in_a_subdirectory(join('data',join(n.identifier.split('/')[-1]))))
+                
+                if n_premis_file_count != n_data_file_count:
+                    current_prefix = n.identifier.split('/')[-1]
+                
+                    return namedtuple("ldrerror", "category message") \
+                        ("fatal","There are {} premis files in {}".format(str(n_premis_file_count), current_prefix) + \
+                         " and there are {} files in corresponding data directory".format(str(n_data_file_count)))
+        
+        for n in data_node:
+            current = data_node.pop()
+            subdirs_in_current = self.find_directories_in_a_directory(current)
+            if len(subdirs_in_current) != self.numfolders:
+                return namedtuple("ldererror","category message") \
+                    ("fatal","There are {} subdirectories in data but there should be {}". \
+                     format(str(len(subdirs_in_current)),str(args.numfolders)))
+            if len(self.find_all_files()) != numfiles:
+                return namedtuple("ldrerror", "category message") \
+                         ("fatal","There are {} files but you said there should be {} files.".format(str(len(self.find_all_files())), args.numfolders))
+
+    def ingest_premis_file(x):
+        print(x)
+
+    def ingest_data_file(x):
+        print(x)
+        
     def ingest(self):
         if self.validate():
-            return "good to ingest"
+    
+            admin_node = self.find_subdirectory_at_particular_level_down('admin',1)
+            data_node = self.find_subdirectory_at_particular_level_down('data', 1)
+            for n in admin_node:
+                current= admin_node.pop()
+                subdirs_in_current = self.find_directories_in_a_directory(current)
+                for i in subdirs_in_current:
+                    current_premis_files = self.find_files_in_a_subdirectory(i)
+                for p in current_premis_files:
+                    ingest_premis_file(p.data.filepath)
+            for n in data_node:
+                current = data_node.pop()
+                subdirs_in_current self.find_directories_in_a_directory(current)
+                for i in subdirs_in_current:
+                    current_data_files = self.find_directories_in_a_directory(i)
+                for d in current_data_files:
+                    ingest_data_file(d.data.filepath)
         else:
-            self.explain_validation_results()
-            return "it's invalid and here's why"
+            return self.explain_validation_results()
+
 
 class Pruner(FileProcessor):
     pattern_inputs = []
