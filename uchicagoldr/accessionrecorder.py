f07836b85341f204710a384a977be9724038cbf2
Brian Balsamo <balsamo@uchicago.edu>
2016-03-08 14:17:51 -0600

    docstrings, support for specifying a minimum number of required children for a type of node in configs.

--- uchicagoldr/accessionrecorder.py
+++ uchicagoldr/accessionrecorder.py
@@ -4,7 +4,25 @@ from hierarchicalrecord.hierarchicalrecord import HierarchicalRecord
 
 
 class AccessionRecorder(object):
+    """
+    AccessionRecorder is a class built to facilitate building and validating
+    accession record files against a CSV format config.
+
+    __Attributes__
+
+    * record (HierarchicalRecord): the record instance to be worked with
+    * conf (AccessionRecordConfig): the config to be used when applicable
+    """
     def __init__(self, record=None, conf=None):
+        """
+        Initialize a new AccessionRecorder instance, potentially with
+        associated record and config values
+
+        __KWArgs__
+
+        * record (HierarchicalRecord): a record instance to associate with
+        * config (AccessionRecordConfig): a config instance to associate with
+        """
         if record:
             self.set_record(record)
         else:
@@ -15,11 +33,35 @@ class AccessionRecorder(object):
             self.conf = None
 
     def _generalize_keys(self, key):
+        """
+        Generalize keys with indexes, for comparison against general dotted
+        syntax
+
+        __Args__
+
+        1. key (str): a str of a dotted key with indices
+
+        __Returns__
+
+        1. result (str): a str of a dotted key with no indices
+        """
         result = subn("\d*$", "", key)[0]
         result = subn("\d*\.", ".", result)[0]
         return result
 
     def _gather_applicable_values(self, field_name):
+        """
+        Gather all values associated with a generalized key
+
+        __Args__
+
+        1. field_name (str): the generalized key/field name
+
+        __Returns__
+
+        1. result (list): a list of the values associated with all instances
+        of the key
+        """
         result = []
         for x in self.get_record().keys():
             comp = self._generalize_keys(x)
@@ -27,7 +69,35 @@ class AccessionRecorder(object):
                 result.append(self.get_record()[x])
         return result
 
+    def _gather_applicable_fields(self, field_name):
+        """
+        Gather all fields associated with a generalized key
+
+        __Args__
+
+        1. field_name (str): the generalized key
+
+        __Returns__
+
+        1. result (list): a list of fields (lists themselves)
+        """
+        result = []
+        for x in self.get_record().keys():
+            comp = self._generalize_keys(x)
+            if comp == field_name:
+                key = subn("\d*$","",x)[0]
+                result.append(self.get_record()[key])
+        return result
+
     def set_record(self, record):
+        """
+        set the record instance associated with this instance
+
+        __Args__
+
+        1. record (HierarchicalRecord or None): A HierarchicalRecord instance to
+        associate with this recorder instance. None to clear it.
+        """
         if record is None:
             self.record = None
             return
@@ -36,9 +106,23 @@ class AccessionRecorder(object):
         self.record = record
 
     def get_record(self):
+        """
+        return the record instance associated with this recorder instance
+
+        __Returns__
+        1. (HierarchicalRecord): the record instance
+        """
         return self.record
 
     def set_conf(self, conf):
+        """
+        set the AccessionRecordConfig instance associated with this instance
+
+        __Args__
+
+        1. conf (AccessionRecordConfig): the instance to be associated with
+        this recorder instance
+        """
         if conf is None:
             self.conf = None
             return
@@ -47,9 +131,28 @@ class AccessionRecorder(object):
         self.conf = conf
 
     def get_conf(self):
+        """
+        return the config instance associated with this recorder instance
+
+        __Returns__
+
+        1. (AccessionRecordConfig): the associated config instance
+        """
         return self.conf
 
     def validate_record(self, strict=True):
+        """
+        validate a record against a config
+
+        __KWArgs__
+
+        * stict: if undocumented fields are in the record fail validation
+
+        __Returns__
+
+        (tuple): a two element tuple containing a bool denoting validation
+        status and potentitally an explanation string
+        """
         if self.get_record() is None:
             raise AttributeError("There is no record associated " +
                                  "with this instance!")
@@ -69,6 +172,7 @@ class AccessionRecorder(object):
             obligation = x['Obligation']
             cardinality = x['Cardinality']
             validation = x['Validation']
+            required_children = x['Children Required']
             nested = False
             if "." in field_name:
                 nested = True
@@ -91,6 +195,14 @@ class AccessionRecorder(object):
                 its_there = False
             if its_there is False:
                 continue
+            if required_children:
+                for x in self._gather_applicable_fields(field_name):
+                    if len(x) < int(required_children):
+                        return (False, "A node does not meet the minimum " +
+                                "requirements for # of children\n" +
+                                "Node: {}\n".format(field_name) +
+                                "Children Detected: {}\n".format(str(len(x))) +
+                                "Required Children: {}".format(required_children))
             if cardinality != 'n':
                 if len(applicable_values) != int(cardinality):
                     return (False, "A nodes cardinality is incorrect.\n" +
@@ -123,6 +235,10 @@ class AccessionRecorder(object):
         return (True, None)
 
     def generate_minimal_record(self):
+        """
+        attempt to generate the shortest/least verbose valid record structure
+        from the associated conf
+        """
         if self.get_record() is not None:
             raise AttributeError("There is already a record associated " +
                                  "with this instance!")
@@ -177,6 +293,10 @@ class AccessionRecorder(object):
                                               leaf_key+"0"] = dummy_value
 
     def generate_full_record(self):
+        """
+        attempt to generate the most verbose valid record from the associated
+        conf
+        """
         if self.get_record() is not None:
             raise AttributeError("There is already a record associated " +
                                  "with this instance!")
@@ -217,6 +337,13 @@ class AccessionRecorder(object):
                         self.get_record()[parent_key+str(y)+"."+leaf_key+str(z)] = dummy_value
 
     def populate_from_csv(self, filepath):
+        """
+        read a csv of key value pairs, populating or altering a record instance
+
+        __Args__
+
+        * filepath (str): the path to a csv file.
+        """
         with open(filepath, 'r') as f:
             reader = DictReader(f)
             for row in reader:
@@ -229,11 +356,33 @@ class AccessionRecorder(object):
 
 
 class AccessionRecordConfig(object):
+    """
+    wraps a csv formatted accession record config, easily exposing the data
+
+    __Attributes__
+
+    * filepath (str): the file path for a config csv
+    * data (list): the config data by row
+    """
     def __init__(self, filepath):
+        """
+        initialize an AccessionRecordConfig instance.
+
+        __Args__
+
+        1. filepath (str): a path to a config csv
+        """
         self.set_filepath(filepath)
         self.set_data(self.read_file())
 
     def read_file(self):
+        """
+        read the associated file path into a list of dictionaries
+
+        __Returns__
+
+        1. rows (list): the list of dictionaries
+        """
         with open(self.filepath, 'r') as f:
             reader = DictReader(f)
             rows = []
@@ -242,13 +391,42 @@ class AccessionRecordConfig(object):
             return rows
 
     def set_filepath(self, filepath):
+        """
+        sets the filepath to the associated csv file
+
+        __Args__
+
+        1. filepath (str): the filepath to a config csv
+        """
         self.filepath = filepath
 
     def get_filepath(self):
+        """
+        returns the filepath associated with the instance
+
+        __Returns__
+
+        1. (str): the filepath associated with the instance
+        """
         return self.filepath
 
     def set_data(self, conf):
+        """
+        set the data associated with a config instance
+
+        __Args__
+
+        1. conf (list): a list of dictionaries to be used as the data of the
+        instance
+        """
         self.conf = conf
 
     def get_data(self):
+        """
+        return the list of config rows
+
+        __Returns__
+
+        1. (list): a list of dictionaries containing config information
+        """
         return self.conf
