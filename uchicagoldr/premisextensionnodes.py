88a86b7e65769bc1b8696531213d71026a173f3b
Brian Balsamo <balsamo@uchicago.edu>
2016-03-28 11:24:38 -0500

    Added checks for conformance to the restriction controlled vocabulary to the restriction premis rights extension node in library code.

--- uchicagoldr/premisextensionnodes.py
+++ uchicagoldr/premisextensionnodes.py
@@ -1,12 +1,39 @@
 from pypremis.nodes import ExtensionNode
+from controlledvocab.lib import ControlledVocabulary as CV
+
 
 class Restriction(ExtensionNode):
+
+    restriction_codes_strs = [
+        "O",
+        "OU",
+        "R-DA",
+        "R-30",
+        "R-50",
+        "R-80",
+        "R-X",
+        "R-P30",
+        "R-S"
+        ]
+
+    restriction_code_patterns = [
+        "^DR-[0-9]+$",
+        "^OO-[0-9]{2}/[0-9]{2}/[0-9]{4}$",
+        "^R-[0-9]+D$",
+        "^MR-.+$"
+        ]
+
+    restriction_code_cv = CV(contains=restriction_codes_strs,
+                             patterns=restriction_code_patterns)
+
     def __init__(self, restrictionCode, active):
         ExtensionNode.__init__(self)
         self.set_restrictionCode(restrictionCode)
         self.set_active(active)
 
     def set_restrictionCode(self, restrictionCode):
+        if restrictionCode not in self.restriction_code_cv:
+            raise ValueError('That is not a valid restriction code')
         self.set_field('restrictionCode', restrictionCode)
 
     def set_active(self, active):
