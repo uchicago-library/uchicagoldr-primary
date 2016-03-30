2b919d757357f3409695fac1dbbe83c2496a65a6
Brian Balsamo <balsamo@uchicago.edu>
2016-03-30 16:18:05 -0500

    More work on the post install script.

--- post_install.py
+++ post_install.py
@@ -77,6 +77,7 @@ def create_prompt(path, dir_or_file):
         else:
             return False
     else:
+        print("{} already exists, and has not been clobbered.".format(path))
         return True
 
 
@@ -84,16 +85,18 @@ def populate_conf_dir(conf_dir):
     pass
 
 
-def first_conf_setup():
+def first_conf_dir_setup():
     conf_dir_suggest = join(BaseDirectory.xdg_config_home, 'ldr')
     conf_dir = PathCLIInput(
         'Config directory location', default=conf_dir_suggest
     ).get_input()
-    while not create_prompt(conf_dir, 'dir'):
-        create_prompt(conf_dir, 'dir')
-    populate_conf_dir(conf_dir)
+    conf_created = create_prompt(conf_dir, 'dir')
+    if not conf_created:
+        print('Configuration can not continue without a target directory. Exiting.')
+        exit(1)
 
     print('Your configuration directory is located at: {}'.format(conf_dir))
+    return conf_dir
 
 
 def main():
@@ -124,8 +127,14 @@ def main():
     print()
     print('To begin, lets determine a location for your configuration ' +
           'directory.')
-
-    first_conf_setup()
+    conf_dir = first_conf_dir_setup()
+    print()
+    print('We will now populate your configuration directory with some ' +
+          'default files. Values set in your user config will override ' +
+          'values in the global config. The global config is located ' +
+          'inside of the python package. Changing them there and ' +
+          'recompiling the package will set global defaults.')
+    conf_file = first_conf_file()
 
 if __name__ == '__main__':
     main()
--- uchicagoldr/ldrconfiguration.py
+++ uchicagoldr/ldrconfiguration.py
@@ -1,41 +1,53 @@
-from configparser import ConfigParser, SectionProxy
-from inspect import getfile, getmodulename, getsourcefile
+from configparser import ConfigParser
+from inspect import getfile
 from os import access, mkdir, R_OK, W_OK
-from os.path import abspath, dirname, expanduser, exists, isdir, join, realpath
-import sys
+from os.path import abspath, exists, join
 from xdg import BaseDirectory
 
 
 class LDRConfiguration(object):
     config_file = None
+    config_dir = None
     data_file = None
     data = {}
 
-    def __init__(self, config_directory=None):
-        if config_directory and exists(abspath(config_directory)):
-            if exists(join(config_directory, 'ldr.ini')):
-                cfg_file = abspath(join(config_directory, 'ldr.ini'))
-            else:
-                raise ValueError('No config file exists in the ' +
-                                 'specified directory')
+    def __init__(self, config_directory=None, config_file=None):
+        if config_directory is None:
+            self.config_dir = self.find_config_dir()
         else:
-            foundOne = False
-            checkedDirs = []
-            for directory in BaseDirectory.load_config_paths('ldr'):
-                checkedDirs.append(directory)
-                if exists(join(directory,'ldr.ini')):
-                    foundOne = True
-                    cfg_file = join(directory,'ldr.ini')
-                    break
-            if foundOne is False:
-                raise ValueError('No config file was found in XDG config ' +
-                                 'directories. The following directories ' +
-                                 'were checked:\n{}'.format(
-                                     '\n'.join(checkedDirs))
-                                 )
-        self.config_file = cfg_file
+            self.config_dir = config_directory
+#        # Handle a conf dir not existing
+#        if self.config_dir is None:
+#            self.spawn_config_dir(self.hypothesize_config_dir_loc())
+        if not exists(self.config_dir):
+            raise ValueError("The config dir does not exist")
+
+        if config_file is None:
+            self.config_file = 'ldr.ini'
+        else:
+            self.config_file = config_file
+        self.config_file_fullpath = join(self.config_dir, self.config_file)
+#        # Handle a conf file not existing
+        if not exists(self.config_file_fullpath):
+#            self.spawn_config_file(self.config_file_fullpath)
+#            raise ValueError('A new config file with default values ' +
+#                             'has been created at {}.\n'.format(
+#                                 self.config_file_fullpath) +
+#                             'Please populate it with appropriate values.')
+            raise ValueError("The primary config file doesn't exist")
+
         self.data_file = self.retrieve_config_data()
         self.data = self.read_config_data()
+
+    def find_config_dir(self):
+        conf_dir = [x for x in BaseDirectory.load_config_paths('ldr')]
+        if conf_dir:
+            return conf_dir[0]
+        else:
+            return None
+
+    def hypothesize_config_dir_loc(self):
+        return join(BaseDirectory.xdg_conf_dirs[0], 'ldr')
 
     def find_config_sections(self):
         output = []
@@ -72,12 +84,42 @@ class LDRConfiguration(object):
         p.set('Logging','port','1')
         return p
 
+    def spawn_config_dir(self, loc):
+        makedirs(loc)
+        makedirs(join(loc, 'controlled_vocabularies'))
+        makedirs(join(loc, 'record_configurations'))
+
+    def spawn_config_file(self, loc):
+        p = ConfigParser()
+        p.add_section('Database')
+        p.add_section('Logging')
+        p.add_section('Paths')
+        p.set('Database','db_host','DB_HOSTNAME_OR_IP_HERE')
+        p.set('Database','db_name','DB_NAME_HERE')
+        p.set('Database','db_user','DB_USERNAME_HERE')
+        p.set('Database','db_pass','DB_USER_PASSWORD_HERE')
+        p.set('Logging','server','LOGGING_HOSTNAME_OR_IP_HERE')
+        p.set('Logging','port','LOGGING_PORT_#_HERE')
+        p.set('Paths', 'staging_root', 'STAGING_ROOT_PATH_HERE')
+        p.set('Paths', 'archive_root', 'ARCHIVE_ROOT_PATH_HERE')
+        p.set('Paths', 'controlled_vocabularies',
+              join(self.config_dir, 'controlled_vocabularies'))
+        p.set('Paths', 'record_configurations',
+              join(self.config_dir, 'record_configurations'))
+        with open(loc, 'w') as f:
+            p.write(f, space_around_delimiters=True)
+
+
+    def spawn_config_parser(self):
+        pass
+
     def retrieve_config_data(self):
         parser = ConfigParser()
-        if exists(abspath(self.config_file)):
-            parser.read(abspath(self.config_file))
+        if exists(abspath(self.config_file_fullpath)):
+            parser.read(abspath(self.config_file_fullpath))
         else:
-            parser = self.write_config_data(parser)
+#            parser = self.write_config_data(parser)
+            raise ValueError()
             cfgfile = open(join(abspath(self.config_file)),'w')
             parser.write(cfgfile)
             cfgfile.close()
