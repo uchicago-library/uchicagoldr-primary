80f4d90de3db424819ee1602d75416b5b9afb1d4
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-03-31 12:11:02 -0500

    bugs!

--- bin/newstaging.py
+++ bin/newstaging.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from collections import namedtuple
 from uchicagoldr.fileprocessor import FileProcessor
 
 if __name__ == "__main__":
@@ -9,6 +10,8 @@ if __name__ == "__main__":
                         type=str, action='store', default='None')
     parser.add_argument("directory", help="The directory that needs to be staged.",
                         type=str, action='store')
+    parser.add_argument("numfiles", help="The number of files that you are expecting to process",
+                        type=int, action='store')
     parser.add_argument("source_root", help="The root of the directory that needs to be staged.",
                         type=str, action='store')
     parser.add_argument("destination_root", help="The location that the staging diectory should be created",
@@ -17,12 +20,12 @@ if __name__ == "__main__":
                         type=str, action='store')
     parser.add_argument("prefix", help="The prefix defining the type of run that is being processed",
                         type=str, action='store')
-    parser.parse_args()
+    args = parser.parse_args()
 
     fp = FileProcessor(args.directory, namedtuple("DirectoryInfo",
-                                                  "src_root dest_root directory_id" +\
-                                                  "prefix directory_type group_name " +\
-                                                  "resume group_name")\
+                                                  "src_root dest_root directory_id prefix " +\
+                                                  "directory_type resume group_name validation")
                        (args.source_root, args.destination_root, args.staging_id,
-                        args.prefix, 'staging', args.resume, args.group))
+                        args.prefix, 'staging', args.resume,
+                        args.group, {'numfiles':args.numfiles}))
     print(fp)
