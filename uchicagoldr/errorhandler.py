9f7eca4a615a15d6b7eea58bc06e15b6c26ca0b5
Brian Balsamo <balsamo@uchicago.edu>
2016-04-04 15:27:57 -0500

    Deleted uchicagoldr dir which snuck back into commits somehow.

--- uchicagoldr/errorhandler.py
+++
@@ -1,9 +0,0 @@
-class ErrorHandler(object):
-    def __init__(self, error):
-        self.error = error
-
-    def handle_error(self):
-        return NotImplemented
-
-    def _handle_value_error(self):
-        return NotImplemented
