c454300a37c7eee1c394e45206ed0699ef5d1206
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-04-15 16:22:53 -0500

    added stagingdirectorywriter

---
+++ uchicagoldrtoolsuite/lib/structuring/abc/stagingserializationwriter.py
@@ -0,0 +1,10 @@
+
+from .serializationwriter import SerializationWriter
+
+
+class StagingSerializationWriter:
+    
+    __metaclass__ = SerializationWriter
+    
+    def __init__(self):
+        pass
---
+++ uchicagoldrtoolsuite/lib/structuring/stagingdirectorywriter.py
@@ -0,0 +1,10 @@
+
+from .abc.serializationwriter import SerializationWriter
+
+class StagingDirectoryWriter(SerializationWriter):
+
+    def __init__(self, aStructure):
+        self.structure = aStructure
+
+
+    
