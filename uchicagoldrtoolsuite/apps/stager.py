e59ee28420622bbfd1c82887dcff0d008a891597
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-04-19 13:32:15 -0500

    finished the segment_number decision tree in stager.py

--- uchicagoldrtoolsuite/apps/stager.py
+++ uchicagoldrtoolsuite/apps/stager.py
@@ -7,6 +7,7 @@ from uchicagoldrtoolsuite.lib.structuring.stagingdirectoryreader import StagingD
 from uchicagoldrtoolsuite.lib.structuring.stagingdirectorywriter import StagingDirectoryWriter
 from uchicagoldrtoolsuite.lib.structuring.stagingdirectorywriter import StagingDirectoryWriter
 from uchicagoldrtoolsuite.lib.structuring.segmentpackager import SegmentPackager
+from uchicagoldrtoolsuite.lib.absolutefilepathtree import AbsoluteFilePathTree
 
 __author__ = "Brian Balsamo, Tyler Danstrom"
 __email__ = "balsamo@uchicago.edu, tdanstrom@uchicago.edu"
@@ -39,7 +40,7 @@ class Stager(CLIApp):
         # Add application specific flags/arguments
         self.parser.add_argument("--resume", "-r", help="An integer for a " +
                                  "run that needs to be resumed.",
-                                 type=int, action='store', default=0)
+                                 type=str, action='store', default=0)
         self.parser.add_argument("--group", "-g", help="The name of a group " +
                                  "to assign group ownership to the new " +
                                  "staging directory",
@@ -74,12 +75,18 @@ class Stager(CLIApp):
         staging_structure = staging_directory_reader.read()
         current_segment = None
         segment_ids = sorted([x.identifier for x in staging_structure.segment])
-        this_prefix_and_number_segment_ids = [x for x in segment_ids if args.prefix+str(args.resume) in x]
+        this_prefix_and_number_segment_ids = [x for x in segment_ids if args.prefix+args.resume in x]
         this_prefix_segment_ids = [x for x in segment_ids if args.prefix in x]
-        print(this_prefix_segment_ids)
+
         if len(this_prefix_and_number_segment_ids) > 0:
+            tree = AbsoluteFilePathTree(args.directory)
+            all_nodes = tree.get_nodes()
+            relevant_segment = [x for x in staging_structure.segment 
+                                if x.identifier == args.prefix+args.resume][0]
+            print(relevant_segment)
             current_segment_number = this_prefix_and_number_segment_ids[-1]
         elif len(this_prefix_segment_ids) > 0:
+            tree = AbsoluteFilePathTree(args.directory)
             current_segment_number = int(re.compile('(\w{1,})(\d{1,})').\
                                  match(this_prefix_segment_ids[-1]).group(2)) + 1
 
@@ -88,8 +95,8 @@ class Stager(CLIApp):
         segment_packager = SegmentPackager(args.directory, args.prefix, 
                                            current_segment_number)
 
-        # new_segment = segment_packager.create_segment()
-        
+        new_segment = segment_packager.create_segment()
+
 
     # def add_to_structure(self, a_directory, prefix, source_root='', number=0):
     #     tree = AbsoluteFilePathTree(a_directory)
