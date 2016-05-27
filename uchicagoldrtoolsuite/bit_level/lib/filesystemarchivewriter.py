24df4bc681f0a05b90cf1c94928d4327fe3de3c9
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-05-27 09:18:41 -0500

    fixed up filesystemarchivewriter. added getters/setters for all new properties

--- uchicagoldrtoolsuite/bit_level/lib/filesystemarchivewriter.py
+++ uchicagoldrtoolsuite/bit_level/lib/filesystemarchivewriter.py
@@ -1,18 +1,12 @@
 
-from datetime import datetime
 from os import makedirs
 from os.path import basename, dirname, exists, join, split
 from sys import stderr
-from tempfile import TemporaryFile
-from xml.etree import ElementTree
 
-from pypremis.lib import PremisRecord
-from pypremis.nodes import Event, EventIdentifier,\
-    EventOutcomeInformation, LinkingAgentIdentifier, LinkingObjectIdentifier,\
-    LinkingEventIdentifier
 from uchicagoldrtoolsuite.core.lib.idbuilder import IDBuilder
 
 from .abc.archiveserializationwriter import ArchiveSerializationWriter
+from .abc.identifier import Identifier
 from .archive import Archive
 from .archivefitsmodifier import ArchiveFitsModifier
 from .archivepremismodifier import ArchivePremisModifier
@@ -86,18 +80,10 @@ class FileSystemArchiveWriter(ArchiveSerializationWriter):
                 segment_id = n_segment.label+'-'+str(n_segment.run)
                 techmd_dir = join(admin_dir, segment_id, 'TECHMD')
                 premis_dir = join(admin_dir, segment_id, 'PREMIS')
-                makedirs(
-                    join(
-                        admin_dir, segment_id), exist_ok=True)
-                makedirs(
-                    join(data_dir, segment_id), exist_ok=True)
-                makedirs(
-                    join(
-                        premis_dir), exist_ok=True)
-                makedirs(
-                    join(
-                        techmd_dir), exist_ok=True)
-
+                segment_data_dir = join(data_dir, segment_id)
+                makedirs(techmd_dir, exist_ok=True)
+                makedirs(premis_dir, exist_ok=True)
+                makedirs(segment_data_dir, exist_ok=True)
                 for n_msuite in n_segment.materialsuite_list:
                     n_content_destination_fullpath = join(
                         data_dir, segment_id, n_msuite.content.item_name)
@@ -177,7 +163,74 @@ class FileSystemArchiveWriter(ArchiveSerializationWriter):
                 output.append((digest_type, digest_value))
         return output
 
+    def get_structure(self):
+        return self._structure
+
+    def set_structure(self, value):
+        if not isinstance(value, Archive):
+            self._structure = value
+        else:
+            raise ValueError(
+                "FileSystemArchiveWriter reqiures an Archive structure" +
+                " in the structure attribute")
+
+    def get_pairtree(self):
+        return self._pairtree
+
+    def set_pairtree(self, value):
+        for n_v in value.split('/'):
+            if len(n_v) == 2:
+                pass
+            else:
+                raise ValueError(
+                    "{} in the pairtree path is ".format(n_v) +
+                    "not a two character string.")
+
+    def get_origin_root(self):
+        return self._origin_root
+
+    def set_origin_root(self, value):
+        if exists(value):
+            self._origin_root = value
+        else:
+            raise ValueError("{} origin root does not exist.".format(value))
+
+    def get_archive_loc(self):
+        return self._archive_loc
+
+    def set_archive_loc(self, value):
+        if exists(value):
+            self._archive_loc = value
+        else:
+            raise ValueError(
+                "{} archive loc does not exist".format(value))
+
+    def get_identifier(self):
+        return self._identifier
+
+    def set_identifier(self, value):
+        if isinstance(value, Identifier):
+            self._identifier = value
+        else:
+            raise ValueError(
+                "{} is not an identifier".format(value))
+
+    def set_premis_modifier(self):
+        return self._premis_modifier
+
+    def get_premis_modifier(self, value):
+        self._premis_modifier = value
+
+    def get_fits_modifier(self):
+        return self._fits_modifier
+
+    def set_fits_modifier(self, value):
+        self._fits_modifier = value
+
     structure = property(get_structure, set_structure)
     pairtree = property(get_pairtree, set_pairtree)
     origin_root = property(get_origin_root, set_origin_root)
     archive_loc = property(get_archive_loc, set_archive_loc)
+    fits_modifer = property(get_fits_modifier, set_fits_modifier)
+    premis_modifier = property(get_premis_modifier, set_premis_modifier)
+    identifier = property(get_identifier, set_identifier)
