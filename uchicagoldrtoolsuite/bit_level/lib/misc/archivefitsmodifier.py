cc2c0dd6e9b7b331558ce8463beb713755741d2b
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-06-06 12:34:01 -0500

    added docstrings to archivefitsmodifier class

--- uchicagoldrtoolsuite/bit_level/lib/misc/archivefitsmodifier.py
+++ uchicagoldrtoolsuite/bit_level/lib/misc/archivefitsmodifier.py
@@ -5,13 +5,34 @@ from xml.etree import ElementTree
 
 from .ldrpath import LDRPath
 
+__author__ = "Tyler Danstrom"
+__email__ = " tdanstrom@uchicago.edu"
+__company__ = "The University of Chicago Library"
+__copyright__ = "Copyright University of Chicago, 2016"
+__publication__ = ""
+__version__ = "0.0.1dev"
+
 
 class ArchiveFitsModifier(object):
+    """
+    The ArchiveFitsModifier is the class for modifiying fits records for ingestion
+    into the ldr archive
+    """
     def __init__(self, record, location_string):
+        """initializes an ArchiveFitsModifier object to be ready to modify the record
+        
+        __Args__
+        1. record (LDRItem): an LDR file-like object pointing to a FITS xml record
+        2. location_string (str): a string representing a location to save the modified
+        xml record
+        """
         self.record = record
         self.location_string = location_string
 
     def modify_record(self):
+        """returns an XML node with a modified filePath and lastmodified 
+        element text
+        """
         root_of_xml_object = self.record.getroot()
         filePath = root_of_xml_object.find(
             '{http://hul.harvard.edu/ois/xml/ns/fits/fits_output}fileinfo/' +
@@ -25,15 +46,26 @@ class ArchiveFitsModifier(object):
         return self.record
 
     def get_location_string(self):
+        """returns the location_string data attribute
+        """
         return self._location_string
 
     def set_location_string(self, value):
+        """sets the location_string data attribute
+        """
         self._location_string = value
 
     def get_record(self):
+        """returns the record data attribute
+        """
         return self._record
 
     def set_record(self, value):
+        """sets the record data attribute value
+        
+        __Args__
+        1. value (LDRItem): a file-like object pointing to a FITS record
+        """
         with TemporaryFile() as tempfile:
             with value.open('rb') as read_file:
                 while True:
