a06c1e4272a05c2c1fe1fdd9fe2d56951df29535
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-06-07 10:43:36 -0500

    added required endpoints to agents/views.py

--- uchicagoldrtoolsuite/server/premis/agents/views.py
+++ uchicagoldrtoolsuite/server/premis/agents/views.py
@@ -7,3 +7,19 @@ blueprint = Blueprint('agents', __name__,
 @blueprint.route("/", methods=['GET'])
 def frontpage():
     return "hi this is a front page of the agents service"
+
+@blueprint.route("/linkAgent", methods=['POST'])
+def linkEventToAgent():
+    return "linking event endpoint"
+
+@blueprint.route("/newAgent", methods=['POST'])
+def addNewAgentRecord():
+    return "new agent endpoint"
+
+@blueprint.route("/retrieveAgent", methods=['GET'])
+def getAnAgentRecord():
+    return "get a specific agent endpoint"
+
+@blueprint.route('/searchAgent', methods=['GET'])
+def searchForAnAgent():
+    return "searching for an agent endpoint"
