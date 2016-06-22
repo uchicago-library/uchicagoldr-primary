d21f744f69c5f2ad266d2093e778ca56deb9993f
Brian Balsamo <balsamo@uchicago.edu>
2016-06-22 15:35:51 -0500

    Fooling with the Acquisition record class and template a bit. Added support for nested record elements.

--- uchicagoldrtoolsuite/server/acquisitions/record/forms/AcquisitionForm.py
+++ uchicagoldrtoolsuite/server/acquisitions/record/forms/AcquisitionForm.py
@@ -3,35 +3,33 @@ from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms.fields.simple import TextAreaField
-from wtforms.fields.core import SelectField, BooleanField
+from wtforms.fields.core import SelectField, BooleanField, IntegerFeild
+from wtforms.fields import FormField
+from wtforms import Form as WTForm
+
+class DonorSourceInfoFields(WTForm):
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    address = StringField("Address")
+    phone = StringField("Phone Number")
+    email = StringField("Email")
+
+class PhysicalMediaFields(WTForm):
+    quantity = IntegerField("Quantity")
+    description = StringField("Description")
 
 class AcquisitionForm(Form):
-    accessionID = StringField("Accession Identifier", 
+    accessionID = StringField("Accession Identifier",
                               validators=[DataRequired])
-    donorfirstname = StringField("Donor's first name", 
-                                 validators=[DataRequired])
-    donorlastname = StringField("Donor's last name", 
-                                validators=[DataRequired])
-    donoremail = StringField("Donor's email address", 
-                             validators=[DataRequired])
-    donorphonenumber = StringField("Donor's phone number", 
-                                   validators=[DataRequired])
-    sourcefirstname = StringField("Source's first name", 
-                                  validators=[DataRequired])
-    sourcelastname = StringField("Source's last name", 
-                                 validators=[DataRequired])
-    sourceemail = StringField("Source's email address", 
-                              validators=[DataRequired])
-    sourcephonenumber = StringField("Source's phone number", 
-                                    validators=[DataRequired])
+    donor = FormField(DonorSourceInfoFields)
+    source = FormField(DonorSourceInfoFields)
     summary = TextAreaField("Description", validators=[DataRequired])
-    restriction = SelectField("Restriction", validators=[DataRequired], 
-                              choices=[('R-X', 'R-X'), 
+    restriction = SelectField("Restriction", validators=[DataRequired],
+                              choices=[('R-X', 'R-X'),
                                        ('R-30', 'R-30'),
                                        ('OU', 'OU'),
                                        ('O','O')])
-    restrictionComment = TextAreaField("Comment about Restriction", 
-                                       validators=[DataRequired])
+    restrictionComment = TextAreaField("Comment about Restriction")
     adminComment = TextAreaField(
         "Administrative Comment")
     physicalMediaNote = TextAreaField(
@@ -47,4 +45,3 @@ class AcquisitionForm(Form):
         "Is a gift acknowledgement required?")
     giftAcknowledgementReceived = BooleanField(
         "Was a gift acknowledgement received?")
-    
