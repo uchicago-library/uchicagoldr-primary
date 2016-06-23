21ab491e52cbf626911b45b6bf414511c7548ae1
Tyler Danstrom <tdanstrom@uchicago.edu>
2016-06-23 10:54:09 -0500

    putting back my changes to AcquisitionForm.py and record.html after merge w/ balsamo's work

--- uchicagoldrtoolsuite/server/acquisitions/record/forms/AcquisitionForm.py
+++ uchicagoldrtoolsuite/server/acquisitions/record/forms/AcquisitionForm.py
@@ -3,40 +3,81 @@ from flask_wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired
 from wtforms.fields.simple import TextAreaField
-from wtforms.fields.core import SelectField, BooleanField, IntegerField
-from wtforms.fields import FormField
+from wtforms.fields.core import SelectField, BooleanField
 from wtforms import Form as WTForm
+from wtforms.fields import FormField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
-class DonorSourceInfoFields(WTForm):
-    first_name = StringField("First Name")
-    last_name = StringField("Last Name")
-    address = StringField("Address")
-    phone = StringField("Phone Number")
-    email = StringField("Email")
+
+def possible_restrictions():
+    from ..models.Restriction import db, Restriction
+    output = db.session.query(Restriction)    
+    return [(x.restrictionCode, x.restrictionCode) for x in output]
 
 class PhysicalMediaFields(WTForm):
-    quantity = IntegerField("Quantity")
-    description = StringField("Description")
+    label = StringField("Descriptive Term", validators=[DataRequired])
+    number = StringField("Amount", validators=[DataRequired])
 
+class DonorFields(WTForm):
+    donor_firstname = StringField("First name", 
+                                 validators=[DataRequired])
+    donor_lastname = StringField("Last name", 
+                                validators=[DataRequired])
+    donor_email = StringField("Email address", 
+                             validators=[DataRequired])
+    donor_phonenumber = StringField("Phone number", 
+                              validators=[DataRequired])    
+
+class SourceFields(WTForm):
+    source_firstname = StringField("First name", 
+                                 validators=[DataRequired])
+    source_lastname = StringField("Last name", 
+                                validators=[DataRequired])
+    source_email = StringField("Email address", 
+                             validators=[DataRequired])
+    source_phonenumber = StringField("Phone number", 
+                              validators=[DataRequired])    
+    
+  
+class OriginFields(WTForm):
+    description = StringField("Description of the origin")
+    
+    
 class RestrictionFields(WTForm):
-    restriction = SelectField("Restriction", validators=[DataRequired],
-                              choices=[('R-X', 'R-X'),
-                                       ('R-30', 'R-30'),
-                                       ('OU', 'OU'),
-                                       ('O','O')])
-    restriction_comment = TextAreaField("Restriction Comment")
-
+    restriction = SelectField('Restriction Code', 
+                                   choices=possible_restrictions(),
+                                   validators=[DataRequired])
+    
+    restrictionComment = TextAreaField("Comment about Restriction", 
+                                       validators=[DataRequired])
+    
 class AcquisitionForm(Form):
-    accessionID = StringField("Accession Identifier",
+    accessionID = StringField("Accession Identifier", 
                               validators=[DataRequired])
     summary = TextAreaField("Description", validators=[DataRequired])
-    restriction_information = FormField(RestrictionFields)
-    donor = FormField(DonorSourceInfoFields)
-    source = FormField(DonorSourceInfoFields)
-    adminComment = TextAreaField("Administrative Comment")
-    physical_media_information = FormField(PhysicalMediaFields)
-    acquisitionType = BooleanField("Is this acquisition all digital?")
+    adminComment = TextAreaField(
+        "Administrative Comment")
+    receiptLetterSent = BooleanField(
+        "Was a letter of receipt sent?")
+    invLetterSent = BooleanField(
+        "Is an inventory letter required?")
     giftAcknowledgementRequired = BooleanField(
         "Is a gift acknowledgement required?")
     giftAcknowledgementReceived = BooleanField(
         "Was a gift acknowledgement received?")
+    acquisitionType = BooleanField("Is this acquisition all digital?")
+
+    physicalLocationNote = TextAreaField(
+        "Note about the physical location")
+
+    collection = StringField("Collection that this belongs in", validators=[DataRequired])
+    donor = FormField(PersonFormFields)
+    source = FormField(PersonFormFields)
+    origin = FormField(OriginFields)
+    
+    restrictions = FormField(RestrictionFields)
+
+    physical_media_info = FormField(PhysicalMediaFields)
+
+
+    
