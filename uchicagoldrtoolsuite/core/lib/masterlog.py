ba4c7a996cc6253df9ec991e18dec5311aea8d77
Brian Balsamo <balsamo@uchicago.edu>
2016-08-03 14:18:59 -0500

    started work on an exceptiohandler to interface with the logger.

--- uchicagoldrtoolsuite/core/lib/masterlog.py
+++ uchicagoldrtoolsuite/core/lib/masterlog.py
@@ -5,19 +5,19 @@ master_log_name = ''
 master_log = getLogger(master_log_name)
 master_log.setLevel('DEBUG')
 
-f = Formatter("[%(levelname)8s] [%(asctime)s] [%(name)s] = " +
-              "%(message)s",
-              datefmt="%Y-%m-%dT%H:%M:%S")
 
-h = StreamHandler()
-h.setLevel('DEBUG')
-h.setFormatter(f)
-
-master_log.addHandler(h)
-
-def spawn_logger(name):
+#def spawn_logger(name, verbosity='INFO'):
+def spawn_logger(name, verbosity='DEBUG',
+                 formatter=Formatter(
+                     "[%(levelname)8s] [%(asctime)s] [%(name)s] = " +
+                     "%(message)s", datefmt="%Y-%m-%dT%H:%M:%S")):
     rname = ''
     if master_log_name:
         rname = rname + master_log_name + "."
     rname = rname + name
-    return getLogger(rname)
+    l = getLogger(rname)
+    h = StreamHandler()
+    h.setFormatter(formatter)
+    h.setLevel(verbosity)
+    l.addHandler(h)
+    return l
